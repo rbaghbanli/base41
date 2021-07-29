feedb3095b1adafc6cb07f9785ad51d32f4a4475
Riad Baghbanli <riad@baghbanli.com>
2021-07-29 16:27:19 -0400

    0.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './export/data-transformation';
-export * from './export/bit-operation';
-export * from './export/hash-code';
-export * from './export/random-number';
+export { default as Data_Transformation } from './export/data-transformation';
+export { default as Bit_Operation } from './export/bit-operation';
+export { default as Hash_Code } from './export/hash-code';
+export { default as Random_Number } from './export/random-number';
