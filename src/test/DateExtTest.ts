9b41902948f638e9bcec793da44e37ebc468fdb1
Riad Baghbanli <rbaghbanli@users.noreply.github.com>
2023-06-23 21:26:56 -0400

    Add tests

--- src/test/DateExtTest.ts
+++ src/test/DateExtTest.ts
@@ -21,3 +21,99 @@ export function testDateTimeString(): number {
 	console.log( `result: passed ${ passed } failed ${ failed }` );
 	return failed;
 }
+
+export function testDateTimeStrings(): number {
+	let passed = 0, failed = 0;
+	console.log( `DateTimeStringTest.testDateTimeStrings started` );
+	[
+		new Date( 0 ),
+		new Date( '2020-01-01' ),
+		new Date( '2020-12-12 06:06' ),
+		new Date( '2020-12-12 06:06:45' ),
+		new Date( '1980-11-30 21:59:59.999' ),
+		new Date( '1980-11-30 21:59:59.999Z' ),
+	].forEach( ( prm, ix ) => {
+		const str: string = Conv.toDateTimeString( prm );
+		const v: string = Conv.toDateTimeString( Conv.fromDateTimeString( str ) );
+		if ( str === v ) {
+			++passed;
+		}
+		else {
+			console.error( `test ${ ix } failed on ${ v } expected ${ str }` );
+			++failed;
+		}
+	} );
+	console.log( `DateTimeStringTest.testDateTimeStrings stopped: passed ${ passed } failed ${ failed }` );
+	return failed;
+}
+
+export function testUtcDateTimeStrings(): number {
+	let passed = 0, failed = 0;
+	console.log( `DateTimeStringTest.testUtcDateTimeStrings started` );
+	[
+		new Date( 0 ),
+		new Date( '2020-01-01' ),
+		new Date( '2020-12-12 06:06' ),
+		new Date( '2020-12-12 06:06:45' ),
+		new Date( '1980-11-30 21:59:59.999' ),
+		new Date( '1980-11-30 21:59:59.999Z' ),
+	].forEach( ( prm, ix ) => {
+		const str: string = Conv.toUtcDateTimeString( prm );
+		const v: string = Conv.toUtcDateTimeString( Conv.fromUtcDateTimeString( str ) );
+		if ( str === v ) {
+			++passed;
+		}
+		else {
+			console.error( `test ${ ix } failed on ${ v } expected ${ str }` );
+			++failed;
+		}
+	} );
+	console.log( `DateTimeStringTest.testUtcDateTimeStrings stopped: passed ${ passed } failed ${ failed }` );
+	return failed;
+}
+
+export function testDateTimeValues(): number {
+	let passed = 0, failed = 0;
+	console.log( `DateTimeStringTest.testDateTimeValues started` );
+	[
+		[ '1920-12-31 22:22:22.999', new Date( '1920-12-31 22:22:22.999' ), true ],
+		[ '2020-01-01 22:22:22.999', new Date( '2020-01-01 22:22:22.999' ), true ],
+		[ '1980-12-31 22:22:22', new Date( '1980-12-31 22:22:22' ), false ],
+		[ '2020-01-01 22:22:22', new Date( '2020-01-01 22:22:22' ), false ],
+	].forEach( ( prm, ix ) => {
+		const str: string = Conv.toDateTimeString( prm[ 1 ] as Date, prm[ 2 ] as boolean );
+		const v: string = prm[ 0 ] as string;
+		if ( str === v ) {
+			++passed;
+		}
+		else {
+			console.error( `test ${ ix } failed on ${ v } expected ${ str }` );
+			++failed;
+		}
+	} );
+	console.log( `DateTimeStringTest.testDateTimeValues stopped: passed ${ passed } failed ${ failed }` );
+	return failed;
+}
+
+export function testUtcDateTimeValues(): number {
+	let passed = 0, failed = 0;
+	console.log( `DateTimeStringTest.testUtcDateTimeValues started` );
+	[
+		[ '1920-12-31 22:22:22.999', new Date( '1920-12-31 22:22:22.999Z' ), true ],
+		[ '2020-01-01 22:22:22.999', new Date( '2020-01-01 22:22:22.999Z' ), true ],
+		[ '1980-12-31 22:22:22', new Date( '1980-12-31 22:22:22.000Z' ), false ],
+		[ '2020-01-01 22:22:22', new Date( '2020-01-01 22:22:22.000Z' ), false ],
+	].forEach( ( prm, ix ) => {
+		const str: string = Conv.toUtcDateTimeString( prm[ 1 ] as Date, prm[ 2 ] as boolean );
+		const v: string = prm[ 0 ] as string;
+		if ( str === v ) {
+			++passed;
+		}
+		else {
+			console.error( `test ${ ix } failed on ${ v } expected ${ str }` );
+			++failed;
+		}
+	} );
+	console.log( `DateTimeStringTest.testUtcDateTimeValues stopped: passed ${ passed } failed ${ failed }` );
+	return failed;
+}
